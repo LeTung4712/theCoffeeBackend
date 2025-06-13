023791e232ddc58669f09c9c4eb653e35658b39d
Le Thanh Tung <iamrobotdiy@gmail.com>
2025-06-13 15:04:25 +0700

    fix: lỗi realtime

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,6 +7,7 @@ use App\Models\Product;
 use App\Models\Topping;
 use App\Models\Voucher;
 use App\Models\VoucherUsage;
+use App\Events\NewOrderEvent;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
